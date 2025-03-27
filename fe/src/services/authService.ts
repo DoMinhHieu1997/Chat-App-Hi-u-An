198aad81644c25353dd0c611452b746abd630aa5
Đỗ Minh Hiếu <minhhieu.do@vietnamnet.vn>
2025-03-27 15:24:16 +0700

    feat: fix chức năng đăng ký

--- fe/src/services/authService.ts
+++ fe/src/services/authService.ts
@@ -1,18 +1,26 @@
-const mainUrl = "http://localhost:5000/api"
+import axios from "axios";
 
-export const registerService = async (name:string, email:string, password:string) => {
-    try {
-        const response = await fetch(mainUrl+"/users/register", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name, email, password }),
-        });
-  
-        if (!response.ok) throw new Error("Đăng ký thất bại!");
-    
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw error;
-    }
+const mainUrl = "http://localhost:5000/api";
+
+export const registerService = async (name: any, email: any, password: any) => {
+  try {
+    // const response = await fetch(mainUrl + "/users/register", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({ name, email, password }),
+    // });
+    const { data } = await axios.post("`${mainUrl}1`users/register", {
+      name,
+      email,
+      password,
+    });
+    console.log(data);
+
+    // if (!response.ok) throw new Error("Đăng ký thất bại!");
+
+    // const data = await response.json();
+    // return data;
+  } catch (error) {
+    console.log(1);
+  }
 };
